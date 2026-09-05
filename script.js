6606e08d81f89d8f76aab39a0b0d40e74e27f8f0
Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>
2026-09-05 10:12:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+
+const list = document.querySelector(".navlist");
+
+hamburger.addEventListener("click", ()=>{
+    list.classList.toggle("list-active")
+})
